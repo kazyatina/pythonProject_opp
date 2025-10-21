5749e2d6014fef8a60c24bb8d3bee8afa1db5c22
olga <koa0308@gmail.com>
2025-10-21 22:28:44 +0400

    добавлены магические методы

--- src/category.py
+++ src/category.py
@@ -18,12 +18,16 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
+    def __str__(self):
+        """Метод для строкового вывода экземпляра класса."""
+        return f"{self.name}, количество продуктов: {len(self.__products)} шт."
+
     @property
     def products(self):
         """Getter, который возвращает строку, информацию по продуктам"""
         prod_list = ""
         for pl in self.__products:
-            prod_list += f"{pl.name}, {pl.price} руб. Остаток: {pl.quantity} шт.\n"
+            prod_list += f"{str(pl)}\n"
         return prod_list
 
     @property
